38a483d51b0157cf74596cc949f41610a2dd3ace
Dan Betcher <dbetcher@hydro.mb.ca>
2015-11-20 08:50:54 -0600

    updated minified version

--- jquery.isfilled-min.js
+++ jquery.isfilled-min.js
@@ -10,4 +10,4 @@
  *	Licensed under the MIT license.
  *	http://en.wikipedia.org/wiki/MIT_License
  */
-(function(e){e.fn.onScreen=function(){var t={top:e(window).scrollTop(),left:e(window).scrollLeft()};t.right=t.left+e(window).width();t.bottom=t.top+e(window).height();var n=this.offset();n.right=n.left+this.outerWidth();n.bottom=n.top+this.outerHeight();return!(t.right<n.left||t.left>n.right||t.bottom<n.top||t.top>n.bottom)}})(jQuery)
+!function(e){e.fn.isFilled=function(i){var r=e(this),t="select"==r.prop("tagName").toLowerCase()?"select":r.attr("type");if(jQuery.inArray(t,["button","submit"])>=0)return void 0;if("checkbox"===t||"radio"===t){if(r.is(":checked")&&void 0===i)return!0;if(void 0!==i&&r.is(":checked")&&r.val()===i)return!0}else{if(""!=r.val()&&void 0===i)return!0;if(void 0!==i&&""!=r.val()&&r.val()===i)return!0}return!1}}(jQuery);
